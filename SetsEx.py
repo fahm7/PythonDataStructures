04adabd42ab398936baa176b6aea249cb59cd270
Faheem <fahm.py@gmail.com>
2019-03-20 12:12:13 +0530

    Sets example

---
+++ SetsEx.py
@@ -0,0 +1,16 @@
+population={ 2, 3, 5, 1, 11, 10,}
+
+
+
+
+
+print("_________________ADD__________________________________")
+countries={'china','india', 'usa','canada'}
+countries.add('london')
+print("Countries after adding london",countries)
+
+countries.add('india')
+
+print("Countries after  adding india",countries)
+
+print("_________________ADD__________________________________")
